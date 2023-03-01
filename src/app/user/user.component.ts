b04262e6ee12564364bff1b32cc1c15b10565d2d
Manirakiza Blaise <manirakizablaise@gmail.com>
2023-03-01 23:30:03 +0200

    Initialisation du projet

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,13 @@
+import { Component, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss'],
+})
+export class UserComponent {
+ constructor(
+  
+ ){
+ }
+}
