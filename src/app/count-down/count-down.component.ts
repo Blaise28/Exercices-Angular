b73bdd26f8f63807030748b49549b52f940bad79
Manirakiza Blaise <manirakizablaise@gmail.com>
2023-03-04 23:02:40 +0200

    parent component calls an @viewChild()

--- src/app/count-down/count-down.component.ts
+++ src/app/count-down/count-down.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { TimerComponent } from './timer/timer.component';
 
 @Component({
   selector: 'app-count-down',
   templateUrl: './count-down.component.html',
-  styleUrls: ['./count-down.component.scss']
+  styleUrls: ['./count-down.component.scss'],
 })
-export class CountDownComponent {
+export class CountDownComponent implements AfterViewInit {
+  @ViewChild(TimerComponent)
+  private timerComponent!: TimerComponent;
+  
+  ngAfterViewInit(): void {
+    setTimeout(() => (this.seconds = () => this.timerComponent.seconds), 0);
+    setTimeout(()=> (this.disabled = () => this.timerComponent.disbled),0); 
+  }
+  seconds(): number {
+    return 0;
+  }
+  disabled():string{
+    return "";
+  }
+  start() {
+    this.timerComponent.start();
+  }
 
+  stop() {
+    this.timerComponent.stop();
+  }
 }
